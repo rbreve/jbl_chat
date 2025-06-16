7c646352faa1c9faab5599c342527aa7fe6f1475
Roberto Breve <rbreve@gmail.com>
2025-06-17 02:22:45 +0300

    Docker setup

--- jbl_chat/jbl_chat/urls.py
+++ jbl_chat/jbl_chat/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -7,4 +8,5 @@ urlpatterns = [
     path("chat/", include("chat.urls")),  # Chat UI routes
     path("api/users/", include("users.urls_api")),  # API routes
     path("api/chat/", include("chat.urls_api")),  # Chat API routes
+    path("", RedirectView.as_view(url="users/")),
 ]
