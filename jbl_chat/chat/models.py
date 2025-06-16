1e979ef2387d9694056635bf2cd85e4df2d19a58
Roberto Breve <rbreve@gmail.com>
2025-06-16 20:43:01 +0300

    fix format black

--- jbl_chat/chat/models.py
+++ jbl_chat/chat/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Chat(models.Model):
-    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages")
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_messages")
+    recipient = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="sent_messages"
+    )
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="received_messages"
+    )
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
